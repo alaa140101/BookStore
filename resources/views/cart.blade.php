b5d26a56708c99011749df415121b13f1250a09e
alaa140101 <alaa140011@gamil.com>
2021-12-14 09:48:30 +0300

    end paypal

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -43,6 +43,7 @@
                     @endforeach
                 </table>
                 <h4>المجموع النهائي: {{ $totalPrice }}</h4>
+                <div id="paypal-button-container" style="width: 20px"></div>
                 @else 
                 <h1>لا يوجد كتب في العربة</h1>
             @endif
@@ -56,5 +57,34 @@
 
 @section('script')
 <script src="https://www.paypal.com/sdk/js?client-id=AV1hLmmCCiDjt3Wo7CuhSmZlba26KmwVdocffVMc0exNpQRVyXGEBlggMVVX_WS9lCPHOWekj8hfunf8"></script>
-<script>paypal.Buttons().render('body');</script>    
+<script>
+paypal.Buttons({
+
+// Sets up the transaction when a payment button is clicked
+createOrder: function(data, actions) {
+  return actions.order.create({
+    purchase_units: [{
+      amount: {
+        value: '20' // Can reference variables or functions. Example: `value: document.getElementById('...').value`
+      }
+    }]
+  });
+},
+
+// Finalize the transaction after payer approval
+onApprove: function(data, actions) {
+  return actions.order.capture().then(function(orderData) {
+    // Successful capture! For dev/demo purposes:
+        console.log('Capture result', orderData, JSON.stringify(orderData, null, 2));
+        var transaction = orderData.purchase_units[0].payments.captures[0];
+        alert('Transaction '+ transaction.status + ': ' + transaction.id + '\n\nSee console for all available details');
+
+    // When ready to go live, remove the alert and show a success message within this page. For example:
+    // var element = document.getElementById('paypal-button-container');
+    // element.innerHTML = '';
+    // element.innerHTML = '<h3>Thank you for your payment!</h3>';
+    // Or go to another URL:  actions.redirect('thank_you.html');
+  });
+}
+}).render('#paypal-button-container');</script>    
 @endsection
